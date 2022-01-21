a1deb859bab2159a5e9d7459bae6ce279a43e926
Peroy Nel <peroynel@yahoo.com>
2022-01-21 13:15:10 +0200

    Update
    Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedinnp } from 'react-icons/ai';
 import { DiTerminal } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
