a96dfe1da03d8bc51184bd9a2666cc87fd36db8c
Peroy Nel <peroynel@yahoo.com>
2022-01-24 15:29:43 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -32,7 +32,7 @@ export const projects = [{
         description: "This application allows users to log in via Email or Google and then to create, edit, like or remove posts from their catalogue. The Front End was done with React and Material-UI. The Back End was built with Node.JS, Express, Axios and MongoDB. The app relies on using  Redux and Reducers to handle state management, aided by Thunk to manage the middleware.",
         image: '/images/1.png',
         tags: ['React', 'Node', 'Express', 'Axios', 'MongoDB', 'Redux'],
-        source: 'https://netlify.com',
+        source: 'https://mern-memories-full-application.netlify.app/posts',
         visit: 'https://github.com/PeroyNel/mern_memories-application',
         id: 3,
     },
